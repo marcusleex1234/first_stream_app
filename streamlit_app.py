f5d740636841e41ffccf8e24ff3a63f5d49d0321
marcusleex1234 <149506609+marcusleex1234@users.noreply.github.com>
2023-11-01 00:10:33 -0800

    Update streamlit_app.py
    Removed EMOJI

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,6 +5,6 @@ streamlit.header('Breakfast Menu')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
-strealit.text('🥑🍞 Avocado Toast')
+strealit.text('🥑 Avocado Toast')
 
-streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+streamlit.header('🥭 Build Your Own Fruit Smoothie 🥝')
