5b8fdcb33fc53fd019d3c8af327362884322f75a
marcusleex1234 <149506609+marcusleex1234@users.noreply.github.com>
2023-11-01 00:15:18 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,6 +5,6 @@ streamlit.header('Breakfast Menu')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
-strealit.text('Avocado Toast')
+streamlit.text('🥑🍞Avocado Toast')
 
-streamlit.header('🥭 Build Your Own Fruit Smoothie 🥝')
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
